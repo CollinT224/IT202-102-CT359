636b4ccd03db8bdd5977a00a8209313789b6cfe8
CollinT22 <61061400+CollinT22@users.noreply.github.com>
2023-02-11 23:27:28 -0500

    Create test.php

---
+++ test.php
@@ -0,0 +1,29 @@
+<html>
+    <body>
+        <title>
+            Sample local page
+        </title>
+        <script> 
+            console.log("Test") 
+            const sample="123"
+            console.log(sample+45)
+
+        </script>
+
+
+
+        <?php
+        print(phpversion())
+        ?>
+        <a href="https://apple.com">apple</a>
+        
+
+        <h2> <?php print("sample") ?>  </h2>
+
+
+
+    </body>
+
+
+
+</html>
